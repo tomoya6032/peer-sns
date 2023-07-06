b761ebb93d444e1c522375a288678295f92103bc
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-07-07 07:09:21 +0900

    テーブルの作成ができた デバイス、ユーザー、ツイートのテーブルができて、htmlの表示もうまくいった 明日から爆速で作っていく

---
+++ app/controllers/toppage_controller.rb
@@ -0,0 +1,45 @@
+class ToppageController < ApplicationController
+
+    def index
+        # render 'toppage/index'
+   
+      end
+   
+   
+      def show
+   
+   
+      end
+   
+   
+      def new
+   
+   
+      end
+   
+   
+      def create
+   
+   
+      end
+   
+   
+      def edit
+   
+   
+      end
+   
+   
+      def update
+   
+   
+      end
+   
+   
+      def destroy
+   
+   
+      end
+   
+
+end
