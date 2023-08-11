d5493b1a2098ce27fabe364ae44b96af508e1b90
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-08-12 07:22:46 +0900

    いいねボタンのAjax化をした でもまだ、show画面のいいねボタンが出てこない それと削除ボタンが機能するようになった それとdisplayネームが表示されないエラーが発生中。

--- db/migrate/20230706212604_create_tweets.rb
+++ db/migrate/20230706212604_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
       t.text :content
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false
 
       t.timestamps
     end
--- db/migrate/20230707214459_create_likes.rb
+++ db/migrate/20230707214459_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :tweet, null: false, foreign_key: true
+      t.references :user, null: false
+      t.references :tweet, null: false
 
       t.timestamps
     end
